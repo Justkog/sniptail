5246901f114fab698c142975e23bad79578f406b
JC <jc@actionaly.com>
2026-01-23 17:04:46 +0100

    Snatchy: implement-1769184178820-d4yha5

--- packages/core/src/config/index.ts
+++ packages/core/src/config/index.ts
@@ -1,2 +1,10 @@
-export { loadBotConfig, loadCoreConfig, loadWorkerConfig, resetConfigCaches } from './env.js';
+export {
+  loadBotConfig,
+  loadCoreConfig,
+  loadWorkerConfig,
+  resetConfigCaches,
+  parseRepoAllowlist,
+  resolveGitHubConfig,
+  resolveGitLabConfig,
+} from './env.js';
 export type { BotConfig, CoreConfig, WorkerConfig } from './env.js';
